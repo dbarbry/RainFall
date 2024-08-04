2008bb556c4d6de290d92e86bcc39f6ef420197d
kbarbry <kellianbarbry@gmail.com>
2024-08-04 22:19:34 +0200

    first push level7

--- level6/source.c
+++ level6/source.c
@@ -1,4 +1,26 @@
-int main(char **av, int ac) {
-    char    *dest;
-    char    **test;
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef void(*ptr)(void);
+
+void    n(void) {
+    system("/bin/cat /home/user/level7/.pass");
+}
+
+void    m(void) {
+    printf("Nope");
+    return;
+}
+
+int main(int ac, char **av) {
+    char    *buffer;
+    ptr     *func_ptr;
+
+    buffer = (char *)malloc(0x40);
+    func_ptr = (void *)malloc(4);
+    *func_ptr = (void *)m;
+    strcpy(buffer, av[1]);
+    (**func_ptr)();
+    return 0;
 }
